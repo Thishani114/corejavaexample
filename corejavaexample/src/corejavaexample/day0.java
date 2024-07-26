31188a18cd930464d188092fb4526dfb557c2c39
HP <HP@LAPTOP-H4M74FFR>
2024-07-26 16:01:10 +0530

    this is my java files

---
+++ corejavaexample/src/corejavaexample/day0.java
@@ -0,0 +1,10 @@
+package corejavaexample;
+
+public class day0 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ corejavaexample/src/corejavaexample/demo.java
@@ -0,0 +1,5 @@
+package corejavaexample;
+
+public class demo {
+
+}
